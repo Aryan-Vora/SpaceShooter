b152fcd10074499374b26a79e29bc8531e803ff9
Aryan Vora <voraaryan1@gmail.com>
2024-05-15 16:30:02 -0700

    added local url so you can quickly open it

--- server.js
+++ server.js
@@ -19,4 +19,5 @@ io.on("connection", (socket) => {
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
+  console.log(`http://localhost:${PORT}`);
 });
